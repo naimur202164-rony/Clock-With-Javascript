451079c598f6343e7e136677358b77e39d860a8e
Your Name <naimur202163@gmail.com>
2022-08-26 15:40:27 +0600

    javascript timer added

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log("Hello From the Server ")
+setInterval(showTime,1000);
+
+const showTime=()=>{
+    let time=new Date();
+    let hour=time.getHours();
+    let min=time.getMinutes();
+    let sec=time.getSeconds();
+    am_pm="AM";
+    if(hour>12){
+        hour=-12;
+        am_pm="PM"
+    }
+}
